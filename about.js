45892a468101113922a50cb6175f846425942149
Michael Nowlin <michaelnowlin5@gmail.com>
2022-03-26 20:08:21 -0600

    working on profile.js now

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+//	console.log('form submit');
+	alert("We have handled your submit.")
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+document.querySelector("img").addEventListener("mouseover", function () {
+	alert("You found me haha.");
+})
---
+++ profile.js
@@ -0,0 +1,7 @@
+let color = document.querySelector('#color')
+
+const showColor = (event) => {
+    alert("Its red man!");
+}
+
+color.addEventListener("click", showColor);
